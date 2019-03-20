61b3cf52db6dbf6797b3248febd55f96c64ac026
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2019-03-20 10:38:25 +0100

    refactor routes member

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
   resources :todos, only: [:new, :create] do
-    post 'complete', on: :member, to: 'completions#create'
-    delete 'incomplete', on: :member, to: 'completions#destroy'
+    member do
+      post 'complete', to: 'completions#create'
+      delete 'incomplete', to: 'completions#destroy'
+    end
   end
   resource :session, only: [:new, :create]
 end
